9b87e81cd13412931af14756b456f214b0315dc4
dmrasf <dmrasf@qq.com>
2020-06-24 22:04:52 +0800

    font2

--- config.h
+++ config.h
@@ -6,13 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char* font = "YaHei Consolas Hybrid:pixelsize=20:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = { 
-    "JoyPixels:pixelsize=10:antialias=true:autohint=true",
-    "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
-    "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true"
-};
-
+static char *font2[] = { "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
